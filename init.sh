2a1cbd29f68d33ff5ba3a4870d9559c1ae42fc72
Ryan McNulty <gtb08177@github.com>
2022-12-17 23:54:40 +0000

    Updated ssh config, introduced Jeff Geerling dotfile role installation, updated Brewfile as a contingency and initialise script to cater for things needing done pre Ansible

---
+++ init.sh
@@ -0,0 +1,7 @@
+# Idea here is that on a fresh Mac, download a zip of this code repo, execute init.sh and you are done
+
+# Before anything will work, we need xcode tools installed
+xcode-select --install
+
+# TODO : Is it then straight into Ansible as that will take care of HomeBrew?
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
